95d3a1e2c9f3122beaf2aad25aac99a24d103e31
Om Patel <omunite21@gmail.com>
2023-09-13 11:29:28 +0530

    Update Quote.js

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -47,7 +47,7 @@ let text = quotes.text + " - " + quotes.author;
   }
 
   return (
-    <div id='quote-box' className="card text-center w-50 position-absolute top-50 start-50 translate-middle">
+    <div id='quote-box' className="card text-center w-75 position-absolute top-50 start-50 translate-middle">
   <div className="card-header fw-semibold">
     The "Quote" Machine
   </div>
